00fbda61bcb11bd1b3f03ee4908d48ea681ae324
MagnusHafstad <hafstadmagnus@gmail.com>
2025-02-10 15:49:43 +0100

    feat: init board

---
+++ Game.py
@@ -0,0 +1,10 @@
+import pygame
+
+class Snake():
+    def __init__(self, size):
+        self.board = [[0 for i in range(size)] for j in range(size)]
+        self.direction = "right"
+        
+
+
+    
---
+++ main.py
@@ -0,0 +1,7 @@
+import Game
+
+def run():
+    print("Hello World!")
+    snake_game = Game.Snake(5)
+
+run()
