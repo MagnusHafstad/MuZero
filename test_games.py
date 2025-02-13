a0e781659d5980d431f39d1a116f9f17b6de1a9a
MagnusHafstad <hafstadmagnus@gmail.com>
2025-02-13 14:18:36 +0100

    Fix: Update test

--- test_games.py
+++ test_games.py
@@ -1,11 +1,12 @@
 import pytest
 import Game
+import numpy as np
 
 def test_setup_game():
     snake_game = Game.Snake(5)
-    assert snake_game.board == [[0,0,0,0,0],
-                                [0,0,0,0,0],
-                                [0,0,0,0,0],
-                                [0,0,0,0,0],
-                                [0,0,0,0,0]]
+    assert np.array_equal(snake_game.board, [[0,0,0,0,0],
+                                             [0,0,0,0,0],
+                                             [1,0,2,0,0],
+                                             [0,0,0,0,0],
+                                             [0,0,0,0,0]])
     assert snake_game.direction == "right"
