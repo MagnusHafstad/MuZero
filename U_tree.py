863b42b305b2e15d54b4d90ad9405ec0eda82305
Nocastle <Ingebomk@stud.ntnu.no>
2025-02-17 13:44:48 +0100

    feat: U_tree

--- U_tree.py
+++ U_tree.py
@@ -1,9 +1,23 @@
 
 class U_tree():
     def __init__(self, abstract_state):
-        self.abstract_state = abstract_state
-        pass
-    pass
+        self.root = Tree_node(abstract_state, None, 0, 0)
+
     
     def search_to_leaf(self):
         pass
+
+
+class Tree_node():
+    def __init__(self, state, parent, reward, depth):
+        self.state = state
+        self.children = []
+        self.parent = parent
+        self.visit_count = 0
+        self.reward = reward
+        self.depth = depth
+
+    def add_child(self, state, parent, reward):
+
+        self.children.append(Tree_node(self, state, parent, reward, self.depth + 1))
+        
