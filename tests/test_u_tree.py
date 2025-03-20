a0fee9907f40289100804687b12b63ef700f3ba9
MagnusHafstad <hafstadmagnus@gmail.com>
2025-03-20 16:18:36 +0100

    test: Add unit test for do_backpropagation in U_tree

--- tests/test_u_tree.py
+++ tests/test_u_tree.py
@@ -28,3 +28,35 @@ def test_add_child():
     assert u_tree.root.children[0].parent == u_tree.root
 
     
+
+def test_do_backpropagation():
+    game = Snake(5)
+    u_tree = U_tree(game.board, 10, [0,1,2,3])
+    new_state, reward =game.simulate_game_step( game.board , 0)
+    u_tree.root.add_child(new_state, u_tree.root,reward, game.status)
+    new_state, reward =game.simulate_game_step( game.board , 0)
+    u_tree.root.children[0].add_child(new_state, u_tree.root.children[0],reward, game.status)
+
+    # Validity of test:
+    assert u_tree.root.reward == 0.0
+    assert u_tree.root.children[0].reward == 10.0
+    assert u_tree.root.children[0].children[0].reward == 20.0
+
+    # Test of do backpropagation
+
+    u_tree.do_backpropagation(u_tree.root.children[0].children[0], 0.0, 0.9)
+
+    # Visit counts should be updated
+    assert u_tree.root.visit_count == 1
+    assert u_tree.root.children[0].visit_count == 1
+    assert u_tree.root.children[0].children[0].visit_count == 1
+
+    # Rewards should be updated
+    assert np.isclose(u_tree.root.children[0].children[0].reward, 20*0.9**0)
+    assert np.isclose(u_tree.root.children[0].reward, 10 + 20 * 0.9**1)
+    assert np.isclose(u_tree.root.reward, 10 * 0.9 + 20 * 0.9**2)
+    
+    
+
+
+test_do_backpropagation()
